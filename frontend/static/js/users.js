0e035eb4afaa972ff73fd6613ca33065b76df1fb
Thai Nguyen <thainv299@gmail.com>
2026-05-21 17:47:01 +0700

    Tăng kích cỡ hình ảnh, đồng nhất giao diện filter

--- frontend/static/js/users.js
+++ frontend/static/js/users.js
@@ -160,7 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>
                         </button>
                         <button class="ds-action-btn delete" data-action="delete" data-id="${user.id}" title="Delete">
-                            <img src="/static/img/delete.png" style="width:18px; height:18px;">
+                            <img src="/static/img/delete.png" style="width:20px; height:20px;" title="Xóa">
                         </button>
                     </div>
                 </td>
